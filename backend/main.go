5b496476c61bf849bde6f0091ed24e53b000cfaf
jorgoose <contact.jorgensen@gmail.com>
2024-02-06 16:02:12 -0600

    HTTPS Support (1)

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-    r := gin.Default()
-    r.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
-    })
-    r.Run(:443)
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	// Use http.ListenAndServeTLS() instead of r.Run()
+	err := http.ListenAndServeTLS(":8080", "server.crt", "server.key", r)
+	if err != nil {
+		panic(err)
+	}
 }
