5a886e3a2f5fb5ceaf9fdec1ef55593d5431c6e9
jorgoose <contact.jorgensen@gmail.com>
2024-02-06 22:10:27 -0600

    Test commit for deployment (11-4)

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"context"
+	"net/http"
 	"github.com/gin-gonic/gin"
 )
 
-// Run server with (unsigned) certificate w/ perm and key
 func main() {
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong, please just work ;[",
 		})
 	})
-	
-	r.RunTLS(":8080", "cert.pem", "key.pem")
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	r.GET("/shutdown", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "shutting down",
+		})
+		go func() {
+			if err := srv.Shutdown(context.Background()); err != nil {
+				panic(err) // failure/timeout shutting down the server gracefully
+			}
+		}()
+	})
+
+	if err := srv.ListenAndServeTLS("cert.pem", "key.pem"); err != nil && err != http.ErrServerClosed {
+		panic(err)
+	}
 }
