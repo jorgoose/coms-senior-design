4aa54896de68946bba116bb31a9be0ae6b617219
jorgoose <contact.jorgensen@gmail.com>
2024-02-06 20:33:40 -0600

    Testing live deployment for server restarting

--- backend/main.go
+++ backend/main.go
@@ -9,7 +9,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong, bading-dong!",
+			"message": "pong (cool we have live deployments now >:D)",
 		})
 	})
 	r.RunTLS(":8080", "cert.pem", "key.pem")
