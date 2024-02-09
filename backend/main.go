2473c4d0ca2ef723d71ab2e69605eb3eebff76f9
Sam <sgm@iastate.edu>
2024-02-09 16:53:12 -0600

    testing out supabase api. Auth will have to be next everything else will be go. To be continued tomorrow

--- backend/main.go
+++ backend/main.go
@@ -4,22 +4,86 @@ import (
 	"backend/utils"
 	"fmt"
 	"context"
+	// "log"
 	"net/http"
 	"github.com/gin-gonic/gin"
+	// "github.com/gofiber/fiber/v2"
+ 	supa "github.com/nedpals/supabase-go"
 )
 
 type Config struct {
 	supabaseKey string
-	supabaseURL string
+	supabaseUrl string
+}
+
+type User struct {
+	Username string `json:"username"` 
+	// AccountType int    `json:"account_type"` 
 }
 
 func main() {
 	r := gin.Default()
 	resourceManager := utils.ResourceManager{};
-	msg := resourceManager.GetProperty("SUPABASE_KEY")
-	fmt.Printf("%s", msg)
+	supabaseKey := resourceManager.GetProperty("SUPABASE_KEY")
+	supabaseUrl := resourceManager.GetProperty("SUPABASE_URL")
+	supabase := supa.CreateClient(supabaseUrl, supabaseKey)
+	// fmt.Printf("%s", msg)
 	// config := ReadConfig()
 	// fmt.Printf("%s: %s\n", config.supabaseKey, config.supabaseURL)
+
+	r.GET("/register", func(c *gin.Context) {
+		user, err := supabase.Auth.SignUp(context.Background(), supa.UserCredentials{
+			Email:    "test1@iastate.edu",
+			Password: "test123wow",
+		})
+		 
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"user": user,
+		})
+	})
+
+	r.POST("/register", func(c *gin.Context) {
+		var row User
+		if err := c.BindJSON(&row); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		
+		  var results []User
+		  err := supabase.DB.From("Games").Insert(row).Execute(&results)
+		  if err != nil {
+			panic(err)
+		  }
+		
+		  fmt.Println(results) // Inserted rows
+	})
+
+
+	r.PUT("/update", func(c *gin.Context) {
+		var row User
+		if err := c.BindJSON(&row); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		
+		  var results []User
+		// Adjust this to match how your Supabase client expects to perform an update operation.
+		err := supabase.DB.From("Users").Update(row).Eq("id", "2a24fb8f-9bd6-4734-99d7-775becf4ec4d").Execute(&results)
+	
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+	
+		c.JSON(http.StatusOK, results)
+	})
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
