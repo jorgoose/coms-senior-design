104633b02eeaeeaa820b01fa3368f2e3ab12eba7
Madhav Dhimal <mpdhimal@iastate.edu>
2024-02-12 21:50:04 +0000

    Update file get_apps.py

--- lambdas/get_apps.py
+++ lambdas/get_apps.py
@@ -1,12 +1,11 @@
 import json
 import requests
 
-#Not Working
+
 def lambda_handler(event, context):
     url = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
     response = requests(url)
-    dataset = response.loads(response.text)
     return {
         'statusCode': 200,
-        'body': json.dumps(dataset)
+        'body': response.json()
     }
