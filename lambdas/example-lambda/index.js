1806b6f99c3ad4ed4cf043f7a57c6119c0c56ac5
jorgoose <contact.jorgensen@gmail.com>
2024-02-14 11:47:41 -0600

    Fixed incorrect file pathing in lambda.yml

--- lambdas/example-lambda/index.js
+++ lambdas/example-lambda/index.js
@@ -2,7 +2,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Auto deployments for Lambda functions are cool'),
+        body: JSON.stringify('Auto deployments for Lambda functions are rad'),
     };
     return response;
 }
