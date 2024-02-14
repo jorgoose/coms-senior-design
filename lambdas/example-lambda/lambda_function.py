42a6099d1bb3d3ed9f5912f0d22094d90e1c6069
jorgoose <contact.jorgensen@gmail.com>
2024-02-14 11:36:54 -0600

    Lambda deployment workflow test

--- lambdas/example-lambda/lambda_function.py
+++ lambdas/example-lambda/lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': 'Lambda auto deployments are pretty rad'
+        'body': 'Lambda auto deployments are pretty rad i guess'
     }
