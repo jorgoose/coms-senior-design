2bd856de9990c6d340509b6a503a5994df1d3913
Elijah Brady <bradyeli09@gmail.com>
2024-02-21 15:58:19 -0800

    Small changes for frontend development

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -17,8 +17,8 @@ export default async function Home() {
     <>
       <div className="bg-gradient-to-r from-stone-500 h-screen w-full flex justify-center items-center flex-col">
         <p className="absolute top-20 text-8xl bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text">TRENDPLAY</p>
-        <Link className="p-5 m-5 w-1/5 h-1/8 bg-gradient-to-r from-blue-400 to-purple-500 rounded-full shadow-2xl text-center" href="/login">Login</Link>
-        <Link className="p-5 m-5 w-1/5 h-1/8 bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text rounded-full text-center" href="/signup">Sign Up</Link>
+        <Link className="p-5 m-5 w-1/5 h-1/8 bg-gradient-to-r from-blue-400 to-purple-500 rounded-full shadow-2xl text-center" href="">DashBoard</Link>
+        <Link className="p-5 m-5 w-1/5 h-1/8 bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text rounded-full text-center" href="">Single Game View</Link>
       </div>
     </>
   );
