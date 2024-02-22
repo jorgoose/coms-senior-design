02ee5daae5b6bafd8e7c1fc50222e6d81daba698
Elijah Brady <bradyeli09@gmail.com>
2024-02-22 11:48:32 -0800

    Did some work on single game view

--- frontend/src/app/gameview/page.tsx
+++ frontend/src/app/gameview/page.tsx
@@ -1,18 +1,20 @@
 import Image from "next/image"
 
 export default function Gameview() {
+
     return (
-        <div className="flex flex-col items-center">
-            <div className="relative h-1/3">
+        <div className="flex flex-col justify-center items-center bg-gradient-to-r from-stone-500">
+            <div className="flex flex-col justify-center items-center w-full m-4">
                 <Image
-                className="relative"
+                className="rounded-lg"
                 src="https://cdn.akamai.steamstatic.com/steam/apps/80/header.jpg?t=1602535977"
                 alt="Example Image"
                 width={500}
                 height={500}
                 />
-                <p className="mt-4">With its extensive Tour of Duty campaign, a near-limitless number of skirmish modes, updates and new content for Counter-Strike's award-winning multiplayer game play, plus over 12 bonus single player missions, Counter-Strike: Condition Zero is a tremendous offering of single and multiplayer content.</p>
             </div>
+            <p className="text-4xl bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text text-center">Counter-Strike: Condition Zero</p>
+            <p className="m-4 bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text text-center">With its extensive Tour of Duty campaign, a near-limitless number of skirmish modes, updates and new content for Counter-Strike's award-winning multiplayer game play, plus over 12 bonus single player missions, Counter-Strike: Condition Zero is a tremendous offering of single and multiplayer content.</p>
         </div>
     );
 }
