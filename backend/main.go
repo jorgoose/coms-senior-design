020106dcb0cab8a4802f3d4cb84a53d66ceb4413
jorgoose <contact.jorgensen@gmail.com>
2024-02-22 14:45:28 -0600

    Minor documentation addition

--- backend/main.go
+++ backend/main.go
@@ -35,6 +35,8 @@ func main() {
 	// 2. The beginning "[]" defines a slice, which is just Go's way of defining a list / dynamic array
 	// 3. "map[string]interface{}" is a map with string keys and interface{} values (interface{} just basically means "any type")
 
+	// Ex: If you used a query like ... .Select("*").Single(), you would use "var res map[string]interface{}" instead since it's only one row
+
 	r.GET("/example-get-request", func(c *gin.Context) {
 		var res []map[string]interface{}
 		err := supabase.DB.From("TestGameEndpoints").Select("*").Execute(&res)
