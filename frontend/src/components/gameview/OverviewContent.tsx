92904b44a9053e0605b2a2e5c65aa84cc47c4c10
Elijah Brady <bradyeli09@gmail.com>
2024-02-26 15:12:41 -0800

    Fixed up some of the spacing and text alignment

--- frontend/src/components/gameview/OverviewContent.tsx
+++ frontend/src/components/gameview/OverviewContent.tsx
@@ -4,9 +4,9 @@ interface OverviewContentProps {
 
 const OverviewContent: React.FC<OverviewContentProps> = ({description}) => {
     return (
-        <div>
-            <p className="m-4 mb-0 text-3xl bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text">Game Description</p>
-            <p className="m-4 mt-2 bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text">{description}</p>
+        <div className="mt-6">
+            <p className="my-4 text-3xl bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text">Game Description</p>
+            <p className="my-4 bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text">{description}</p>
         </div>
     );
 }
