ebf1fd61e3cac021e6ea7911436eee29ce754e3d
Elijah Brady <bradyeli09@gmail.com>
2024-03-09 13:38:06 -0800

    Added a small amount of error handling

--- frontend/src/api/games.ts
+++ frontend/src/api/games.ts
@@ -2,5 +2,8 @@ import axios from "axios";
 
 export async function getAllGames() {
     const data = await axios.get(`${process.env.NEXT_PUBLIC_EC2_SERVER}/get-all-games`);
+    if (!data) {
+        throw "Error in fetching data"
+    }
     return data;
 }
