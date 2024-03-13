0f82a349e544cc3ba9f2c1d7f2229f9c54b2388a
Elijah Brady <bradyeli09@gmail.com>
2024-03-13 15:00:07 -0700

    Added call to fetch a single game by appid, also adjusted TestComp

--- frontend/src/api/games.ts
+++ frontend/src/api/games.ts
@@ -1,9 +1,20 @@
 import axios from "axios";
 
+const baseURL = process.env.NEXT_PUBLIC_EC2_SERVER
+
 export async function getAllGames() {
-    const data = await axios.get(`${process.env.NEXT_PUBLIC_EC2_SERVER}/get-all-games`);
+    const data = await axios.get(`${baseURL}/get-all-games`);
     if (!data) {
-        throw "Error in fetching data"
+        throw "Error in fetching all games";
+    }
+    return data;
+}
+
+// localhost:8080/request-game?AppID=80
+export async function getGame(appId: number) {
+    const data = await axios.get(`${baseURL}/request-game?AppID=${appId}`);
+    if (!data) {
+        throw "Error in fetching a specific game";
     }
     return data;
 }
--- frontend/src/components/TestComp.tsx
+++ frontend/src/components/TestComp.tsx
@@ -1,23 +1,23 @@
 'use client'
 
 import React, { useEffect, useState } from 'react';
-import { getAllGames } from '@/api/games';
+import { getAllGames, getGame } from '@/api/games';
 
 function TestComp() {
     const [games, setGames] = useState([]);
 
     useEffect(() => {
         // Fetch games when component mounts
-        async function fetchGames() {
+        async function fetchGame() {
             try {
-                const response = await getAllGames();
+                const response = await getGame(80);
                 setGames(response.data); // Assuming the response contains game data
             } catch (error) {
                 console.error('Error fetching games:', error);
             }
         }
 
-        fetchGames();
+        fetchGame();
     }, []);
 
     return (
