891a2311926645990a7858fb22e46690527f3184
Sam <sgm@iastate.edu>
2024-03-14 09:41:44 -0500

    added side bar menu component to help abstract some things

--- frontend/src/components/DashboardComp.tsx
+++ frontend/src/components/DashboardComp.tsx
@@ -1,15 +1,12 @@
 'use client';
 
-import Link from "next/link";
-import GamepadIcon from "./icons/GamepadIcon";
-import { SetStateAction, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
-import SideBarComp from "./SideBarComp";
-import BellIcon from "./icons/BellIcon"
 import DropdownComp from "@/components/DropdownComp";
 import SearchBar from "@/components/SearchBar";
 import GameCard from "@/components/GameCardComp";
 import { getAllGames } from '@/api/games';
+import SidebarMenuComp from "./SidebarMenuComp";
 
 const DashboardComp: React.FC<{}> = () => {
 
@@ -36,21 +33,7 @@ const DashboardComp: React.FC<{}> = () => {
 
     return (
         <>
-            <div className="fixed top-0 left-0 h-screen w-[280px] border-r border-stone-700 bg-stone-800 overflow-auto lg:block">
-                <div className="flex flex-col gap-2">
-                    <div className="flex h-[60px] items-center border-b border-stone-700 px-5">
-                        <Link className="flex items-center gap-2 font-semibold" href="#">
-                            <GamepadIcon className="h-6 w-6" />
-                            <span className="text-xl text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500">TrendPlay</span>
-                        </Link>
-                        <button className="ml-auto h-8 w-8 text-transparent">
-                            <BellIcon className="h-5 w-5" />
-                            <span className="sr-only">Toggle notifications</span>
-                        </button>
-                    </div>
-                    <SideBarComp  />
-                </div>
-            </div>
+            <SidebarMenuComp />
             <div className="flex flex-col w-full lg:pl-[280px]">
                 <header className="fixed top-0 z-10 w-full h-14 lg:h-[60px] border-b border-stone-700 bg-stone-800 px-6 flex items-center gap-4">
                     <div className="w-full flex-1">
