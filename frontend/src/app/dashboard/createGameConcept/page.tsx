8222c8d3dc633d9e3dc3e1c10b36f4d90a33a31c
Elijah Brady <bradyeli09@gmail.com>
2024-03-18 11:54:35 -0700

    Built out the stub for creating game concepts and did a little cleanup in some files

--- frontend/src/app/dashboard/createGameConcept/page.tsx
+++ frontend/src/app/dashboard/createGameConcept/page.tsx
@@ -2,8 +2,9 @@ import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 import { createClient } from '@/utils/supabase/server'
+import CreateGameConceptComp from '@/components/pageComps/CreateGameConceptComp';
 
-export default async function CreateGame() {
+export default async function CreateGameConcept() {
     const cookieStore = cookies();
     const supabase = createClient(cookieStore);
   
@@ -13,8 +14,10 @@ export default async function CreateGame() {
     }
 
     return (
-        <main>
-            
-        </main>
+      <>
+        <div className="flex min-h-screen w-full bg-gradient-to-r from-stone-500 text-stone-200">
+            <CreateGameConceptComp />
+        </div>
+      </>
     );
 }
