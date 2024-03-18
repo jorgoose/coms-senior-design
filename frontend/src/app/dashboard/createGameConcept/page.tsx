64a88ce1114461ce1f6db644723a34b2eb2599a5
Elijah Brady <bradyeli09@gmail.com>
2024-03-18 15:11:29 -0700

    Fix white space

--- frontend/src/app/dashboard/createGameConcept/page.tsx
+++ frontend/src/app/dashboard/createGameConcept/page.tsx
@@ -5,21 +5,21 @@ import { createClient } from '@/utils/supabase/server'
 import CreateGameConceptComp from '@/components/pageComps/CreateGameConceptComp';
 
 export default async function CreateGameConcept() {
-    const cookieStore = cookies();
-    const supabase = createClient(cookieStore);
-  
-    const { data, error } = await supabase.auth.getUser();
-    if (error || !data?.user) {
-      redirect('/login');
-    }
-    const currUser = data.user;
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
 
-    return (
-      <>
-        <div className="flex min-h-screen w-full bg-gradient-to-r from-stone-500 text-stone-200">
-            <CreateGameConceptComp
-              user={currUser}/>
-        </div>
-      </>
-    );
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    redirect('/login');
+  }
+  const currUser = data.user;
+
+  return (
+    <>
+      <div className="flex min-h-screen w-full bg-gradient-to-r from-stone-500 text-stone-200">
+        <CreateGameConceptComp
+          user={currUser}/>
+      </div>
+    </>
+  );
 }
