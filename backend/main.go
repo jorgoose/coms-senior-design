91333c0e315f280cac7ba8414960358b83c8086c
Madhav314 <mdhimal314@gmail.com>
2024-03-19 16:23:30 -0500

    small name change

--- backend/main.go
+++ backend/main.go
@@ -352,7 +352,7 @@ func main() {
 	// Gets all games concepts that fit the given query
 	// TODO : need to add more filters when nessessary
 	// Ex. http://localhost:8080/Filter-Game-concept/?Genres=Free to Play,Action&...
-	r.GET("/filter-Game-concept", func(c *gin.Context) {
+	r.GET("/filter-game-concept", func(c *gin.Context) {
 		var res []map[string]interface{}
 
 		Genres := c.Query("Genre")
