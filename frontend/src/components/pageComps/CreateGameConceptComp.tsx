92408b1ff0c892965516f384ecdbf03e8497c196
Elijah Brady <bradyeli09@gmail.com>
2024-03-20 09:41:56 -0700

    Fix whitespace

--- frontend/src/components/pageComps/CreateGameConceptComp.tsx
+++ frontend/src/components/pageComps/CreateGameConceptComp.tsx
@@ -26,26 +26,26 @@ const CreateGameConceptComp: React.FC<CreateGameConceptProps> = ({ user }) => {
     return (
         <>
             <LayoutComponent searchQuery={''} setSearchQuery={() => {}} showSearchBar={false}>
-                    <main className="flex flex-col md:gap-8 pr-5 pt-[50px] lg:pt-[60px] overflow-auto">
-                        <form action={async (formData: FormData) => {
-                            if (user.id) {
-                                formData.append("developer_id", user.id)
-                                formAction(formData);
-                            }
-                        }}>
-                            <input className="rounded-md w-1/2 text-black" name="title" placeholder="Title" required />
-                            <input className="rounded-md w-1/2 text-black" name="description" placeholder="Description" required />
-                            <input className="rounded-md w-1/2 text-black" name="genre" placeholder="Genre" required />
-                            <input className="rounded-md w-1/2 text-black" name="tags" placeholder="Tags" required />
-                            <p aria-live="polite">
-                                {state?.message}
-                            </p>
-                            <SubmitButton use='Submit' />
-                        </form>
-                        <div>
-                            <TagList tags={tags} onTagClick={handleTagSelection} />
-                        </div>
-                    </main>
+                <main className="flex flex-col md:gap-8 pr-5 pt-[50px] lg:pt-[60px] overflow-auto">
+                    <form action={async (formData: FormData) => {
+                        if (user.id) {
+                            formData.append("developer_id", user.id)
+                            formAction(formData);
+                        }
+                    }}>
+                        <input className="rounded-md w-1/2 text-black" name="title" placeholder="Title" required />
+                        <input className="rounded-md w-1/2 text-black" name="description" placeholder="Description" required />
+                        <input className="rounded-md w-1/2 text-black" name="genre" placeholder="Genre" required />
+                        <input className="rounded-md w-1/2 text-black" name="tags" placeholder="Tags" required />
+                        <p aria-live="polite">
+                            {state?.message}
+                        </p>
+                        <SubmitButton use='Submit' />
+                    </form>
+                    <div>
+                        <TagList tags={tags} onTagClick={handleTagSelection} />
+                    </div>
+                </main>
             </LayoutComponent>
         </>
     );
