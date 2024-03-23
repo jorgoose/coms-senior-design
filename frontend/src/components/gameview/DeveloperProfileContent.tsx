750352b7a9c29483c7adfc1d24aa0ee342947578
Sam <sgm@iastate.edu>
2024-03-23 14:28:12 -0500

    added slider

--- frontend/src/components/gameview/DeveloperProfileContent.tsx
+++ frontend/src/components/gameview/DeveloperProfileContent.tsx
@@ -16,6 +16,10 @@ const DeveloperProfileContent: React.FC<DeveloperProfileContentProps> = ({ game 
                 const response = await getGamesByDeveloper(developerArgs);
                 console.log(response.data);
                 setOtherGames(response.data);
+                // TEST SCROLLING
+                // const data = response.data;
+                // const duplicatedData = [...data, ...data, ...data, ...data];
+                // setOtherGames(duplicatedData);
                 
             } catch(error1) {
                 console.error('Bad Response', error1);
@@ -31,24 +35,28 @@ const DeveloperProfileContent: React.FC<DeveloperProfileContentProps> = ({ game 
 
     return (
         <>
-            <div className="p-4 bg-stone-800 rounded-xl">
-                <p className="text-3xl bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text">Game Developer</p>
-                <p className="bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text">{game.Developers}</p>
+            <div className="p-4 bg-stone-800 rounded-xl max-w-fit">
+                <p className="text-3xl bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text">Game Developer: {game.Developers}</p>
+                {/* <p className="text-2xl bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text"></p> */}
             </div>
-            {(otherGames.length > 1 && <div className="mt-6 p-4 bg-stone-800 rounded-xl">
-                <p className="text-2xl bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text">Other Games by this Devloper</p>
-                {otherGames.map((devGame) => (
-                    (game.AppID != devGame.AppID && <div key={devGame.AppID}>
-                        <GameCard 
-                            key={devGame.AppID}   
-                            game={devGame} 
-                            onClick={()=> {}} 
-                            showFavorite={false}
-                        />
+            {(otherGames.length > 1) && (
+                <div className="mt-6 p-4 bg-stone-800 rounded-xl overflow-x-auto">
+                    <p className="text-2xl bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text">Other Games by this Devloper</p>
+                    <div className="flex space-x-4 mt-4 max-w-screen-lg">
+                        {otherGames.map((devGame) => (
+                            (game.AppID != devGame.AppID && <div key={devGame.AppID}>
+                                <GameCard 
+                                    key={devGame.AppID}   
+                                    game={devGame} 
+                                    onClick={()=> {}} 
+                                    showFavorite={false}
+                                />
+                            </div>
+                            )
+                        ))}
                     </div>
-                    )
-                ))}
-            </div>)}
+                </div>
+            )}
         </>
     );
 }
