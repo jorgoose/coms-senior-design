a6778746cc312c6071e7bd6aca185c6b26a5f8d9
Elijah Brady <bradyeli09@gmail.com>
2024-03-24 13:56:44 -0700

    Create stub for new page

---
+++ frontend/src/components/pageComps/GameConceptsComp.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import LayoutComponent from "../header/LayoutComponent";
+
+interface GameConceptsCompProps {
+    UserID: string;
+}
+
+const GameConceptsComp: React.FC<GameConceptsCompProps> = ({ UserID }) => {
+
+    return (
+        <>
+            <LayoutComponent searchQuery={''} setSearchQuery={() => {}} showSearchBar={false}>
+                <main className="flex flex-col md:gap-8 pr-5 pt-[50px] lg:pt-[60px] overflow-auto">
+                    Game Concepts Comp
+                </main>
+            </LayoutComponent>
+        </>
+    );
+};
+
+export default GameConceptsComp;
