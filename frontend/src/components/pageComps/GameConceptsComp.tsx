33eeb3fb4d73797d0e233124e13490c0499caa9d
Elijah Brady <bradyeli09@gmail.com>
2024-03-25 08:08:43 -0700

    Add some handling for things not being there

--- frontend/src/components/pageComps/GameConceptsComp.tsx
+++ frontend/src/components/pageComps/GameConceptsComp.tsx
@@ -34,22 +34,21 @@ const GameConceptsComp: React.FC<GameConceptsCompProps> = ({ UserID }) => {
         <>
             <LayoutComponent searchQuery={searchQuery} setSearchQuery={setSearchQuery} showSearchBar={true}>
                 <main className="flex flex-col md:gap-8 pr-5 pt-[50px] lg:pt-[60px] overflow-auto">
-                    <p className="text-sky-500">Game Concepts</p>
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
                         {filteredConcepts.map((concept, index) => (
                             <div key={index} className="relative">
                                 <div className="w-56 h-44 bg-stone-800 rounded-t overflow-auto shadow-lg relative">
                                     <p className="ml-2">Title</p>
-                                    <p className="m-2 mt-0 text-sky-500">{concept.Title}</p>
+                                    <p className="m-2 mt-0 text-sky-500">{concept.Title ? concept.Title : 'No Title Given'}</p>
                                     <p className="ml-2">Description</p>
-                                    <p className="m-2 mt-0 text-sky-500">{concept.Description}</p>
+                                    <p className="m-2 mt-0 text-sky-500">{concept.Description ? concept.Description : 'No Description Given'}</p>
                                     <p className="ml-2">Genres</p>
-                                    <p className="m-2 mt-0 text-sky-500">{concept.Genres ? concept.Genres.join(', ') : ''}</p>
+                                    <p className="m-2 mt-0 text-sky-500">{concept.Genres ? concept.Genres.join(', ') : 'No Genres Given'}</p>
                                     <p className="ml-2">Tags</p>
-                                    <p className="m-2 my-0 text-sky-500">{concept.Tags ? concept.Tags.join(', '): ''}</p>
+                                    <p className="m-2 my-0 text-sky-500">{concept.Tags ? concept.Tags.join(', '): 'No Tags Given'}</p>
                                 </div>
                                 <div className="w-56 bg-stone-800 rounded-b shadow-lg relative">
-                                    <input type="text" className="bg-slate-600 m-2" placeholder="Leave Review"/>
+                                    <input type="text" className="bg-slate-600 m-2 rounded-md" placeholder="Leave Review"/>
                                     <button className="ml-2">Submit</button>
                                 </div>
                             </div>
