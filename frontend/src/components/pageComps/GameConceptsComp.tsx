a91138180d17092290b180fc022e9aab71465a54
Elijah Brady <bradyeli09@gmail.com>
2024-03-25 11:30:17 -0700

    Getting out of the trenches, got some fancy css goin on

--- frontend/src/components/pageComps/GameConceptsComp.tsx
+++ frontend/src/components/pageComps/GameConceptsComp.tsx
@@ -20,6 +20,7 @@ const GameConceptsComp: React.FC<GameConceptsCompProps> = ({ UserID }) => {
     const [state, formAction] = useFormState(postReview, initialState);
     const [searchQuery, setSearchQuery] = useState('');
     const [gameConcepts, setGameConcepts] = useState<GameConcept[]>([]);
+    const [currCardIndex, setCurrCardIndex] = useState(-1);
 
     const filteredConcepts = gameConcepts.filter((concept) =>
         concept?.Title?.toLowerCase().includes(searchQuery.toLowerCase())
@@ -39,14 +40,18 @@ const GameConceptsComp: React.FC<GameConceptsCompProps> = ({ UserID }) => {
         (UserID) && fetchGameConcepts(UserID);
     }, []);
 
+    const handleCardClick = (index: number) => {
+        setCurrCardIndex(index === currCardIndex ? -1 : index);
+    }
+
     return (
         <>
             <LayoutComponent searchQuery={searchQuery} setSearchQuery={setSearchQuery} showSearchBar={true}>
                 <main className="flex flex-col md:gap-8 pr-5 pt-[50px] lg:pt-[60px] overflow-auto">
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+                    <div className={`grid ${currCardIndex !== -1 ? 'grid-flow-row grid-cols-5 gap-2' : 'grid-flow-row grid-cols-5 gap-4'}`}>
                         {filteredConcepts.map((concept, index) => (
-                            <div key={index} className="relative">
-                                <div className="w-56 h-44 bg-stone-800 rounded-t overflow-auto shadow-lg relative">
+                            <div key={index} onClick={() => handleCardClick(index)} className={`bg-stone-800 rounded-t overflow-auto shadow-lg relative ${index === currCardIndex ? 'w-full h-120 col-span-5' : 'w-56 h-60'}`}>
+                                <div>
                                     <p className="ml-2">Title</p>
                                     <p className="m-2 mt-0 text-sky-500">{concept.Title ? concept.Title : 'No Title Given'}</p>
                                     <p className="ml-2">Description</p>
@@ -56,7 +61,7 @@ const GameConceptsComp: React.FC<GameConceptsCompProps> = ({ UserID }) => {
                                     <p className="ml-2">Tags</p>
                                     <p className="m-2 my-0 text-sky-500">{concept.Tags ? concept.Tags.join(', '): 'No Tags Given'}</p>
                                 </div>
-                                <div className="w-56 bg-stone-800 rounded-b border-t border-t-indigo-500 shadow-lg relative">
+                                <div>
                                     <form action={async (formData: FormData) => {
                                         formData.append("UserID", UserID);
                                         if (concept.id) formData.append("ConceptID", concept.id);
@@ -64,6 +69,9 @@ const GameConceptsComp: React.FC<GameConceptsCompProps> = ({ UserID }) => {
                                         redirect('/dashboard/gameConcepts');
                                     }}>
                                         <input type="text" className="bg-slate-600 m-2 rounded-md" name="comment" placeholder="Leave Review"/>
+                                        <p aria-live="polite">
+                                            {state?.message}
+                                        </p>
                                         <SubmitButton use='Submit' />
                                     </form>
                                 </div>
