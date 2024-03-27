9bf1f555f68a206838649d27c49d3801f1e8ea0c
jorgoose <contact.jorgensen@gmail.com>
2024-03-27 12:22:19 -0500

    Refactor SubmitButton component to allow custom styles through props (default, on hover, and pending)

--- frontend/src/components/SubmitButton.tsx
+++ frontend/src/components/SubmitButton.tsx
@@ -1,19 +1,31 @@
 'use client'
- 
 import { useFormStatus } from 'react-dom'
 
 interface SubmitButtonProps {
-    use: string;
+  use: string;
+  customStyles?: {
+    default: string;
+    hover: string;
+    pending: string;
+  };
 }
 
-const SubmitButton: React.FC<SubmitButtonProps> = ({ use }) => {
-    const { pending } = useFormStatus();
+const SubmitButton: React.FC<SubmitButtonProps> = ({ use, customStyles }) => {
+  const { pending } = useFormStatus();
 
-    return (
-        <button type="submit" aria-disabled={pending} className="bg-blue-500 text-white border border-blue-500 rounded-lg px-4 py-2 m-2">
-            {pending ? 'Loading...' : use}
-        </button>
-    );
+  const defaultStyles = customStyles?.default || 'bg-blue-500 text-white border border-blue-500 rounded-lg px-4 py-2 m-2';
+  const hoverStyles = customStyles?.hover || '';
+  const pendingStyles = customStyles?.pending || 'bg-blue-500 text-white border border-blue-500 rounded-lg px-4 py-2 m-2';
+
+  return (
+    <button
+      type="submit"
+      aria-disabled={pending}
+      className={`${defaultStyles} ${pending ? pendingStyles : hoverStyles}`}
+    >
+      {pending ? 'Loading...' : use}
+    </button>
+  );
 };
-  
+
 export default SubmitButton;
